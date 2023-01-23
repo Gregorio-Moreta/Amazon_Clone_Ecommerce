c34d964b79452f35c32fafe3d2ef9a5a3b8f028d
Gregorio-Moreta <78994816+Gregorio-Moreta@users.noreply.github.com>
2023-01-23 10:54:48 -0500

    Tailwind CSS installed, up and running with hello world rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import './input.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>hello worldy</p>
+        <h1 class="text-3xl font-bold underline">
+        Hello world!
+        </h1>
       </header>
     </div>
   );
